e9f6565ec5977c7b1d7458d2a2922ebbc4d2270a
jslim1995 <j.liiim95@gmail.com>
2024-04-05 10:05:55 +0900

    VPC 구성 코드

---
+++ main/variables.tf
@@ -0,0 +1,76 @@
+variable "prefix" {
+  default     = "terraform_jinsu"
+  description = "prefix"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS ACCESS KEY"
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS SECRET ACCESS KEY"
+  sensitive   = true
+}
+
+variable "aws_region" {
+  default     = "ca-central-1"
+  # default     = "ap-northeast-2"
+  description = "aws region"
+}
+
+variable "aws_vpc_cidr" {
+  default     = "172.170.0.0/16"
+  description = "AWS VPC CIDR"
+}
+
+### az, cidr 설정
+variable "map_subnet_az" {
+  type = map(
+    list(
+      object(
+        {
+          availability_zone = string
+          # cidr_block        = string
+        }
+      )
+    )
+  )
+
+  default = {
+    ## ca-central-1
+    "ca-central-1" = [
+      {
+        availability_zone = "ca-central-1a"
+        # cidr_block        = cidrsubnets(var.aws_vpc_cidr, 8, 8, 8, 8)[0]
+      },
+      {
+        availability_zone = "ca-central-1b"
+        # cidr_block        = cidrsubnets(var.aws_vpc_cidr, 8, 8, 8, 8)[1]
+      },
+    ],
+
+    ## ap-northeast-2
+    "ap-northeast-2" = [
+      {
+        availability_zone = "ap-northeast-2a"
+        # cidr_block        = cidrsubnets(var.aws_vpc_cidr, 8, 8, 8, 8)[0]
+      },
+      {
+        availability_zone = "ap-northeast-2c"
+        # cidr_block        = cidrsubnets(var.aws_vpc_cidr, 8, 8, 8, 8)[1]
+      },
+      {
+        availability_zone = "ap-northeast-2d"
+        # cidr_block        = cidrsubnets(var.aws_vpc_cidr, 8, 8, 8, 8)[2]
+      },
+    ]
+  }
+
+  description = "Subnet AZ 설정 값 목록"
+}
+
+variable "pem_key_name" {
+  default     = "jinsu"
+  description = "AWS PEM KEY 명"
+}
