eee4f1b10a217ccc504a133178810b1dbe76bc84
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-06-03 13:57:15 +0900

    remote mode 설정

--- tfe_fdo/provider.tf
+++ tfe_fdo/provider.tf
@@ -5,3 +5,16 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+
+
+# remote 실행모드 설정
+# terraform login 필요
+terraform {
+  cloud {
+    organization = "insideinfo_jinsu"
+
+    workspaces {
+      name = "tfe_fdo"
+    }
+  }
+}
