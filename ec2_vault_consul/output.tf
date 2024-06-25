b5b80e1172e43f0c0cd265ed6a50e14d2fccf7a4
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-06-25 15:54:35 +0900

    각 인스턴스 public ip 값 출력

--- ec2_vault_consul/output.tf
+++ ec2_vault_consul/output.tf
@@ -1,5 +1,9 @@
 output "vault_public_ip" {
-  value = aws_instance.vault_raft_amz2.*.public_ip
+  value = aws_instance.vault_consul_amz2.*.public_ip
+}
+
+output "consul_public_ip" {
+  value = aws_instance.consul_amz2.*.public_ip
 }
 
 output "ami" {
