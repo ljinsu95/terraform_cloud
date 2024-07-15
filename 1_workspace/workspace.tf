11ca7edd6abd544ca1288d64c76c59ccd1f88322
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-07-15 13:40:10 +0900

    tfc_poc_workspace assessments_enabled 옵션 비활성화

--- 1_workspace/workspace.tf
+++ 1_workspace/workspace.tf
@@ -23,22 +23,22 @@ resource "tfe_workspace" "tfc_poc_workspace" {
   queue_all_runs        = false
   working_directory     = each.value
   global_remote_state   = true
-  assessments_enabled   = true
+  assessments_enabled   = false # 상태 평가 옵션, HCP Plus 혹은 TFE 필요 https://developer.hashicorp.com/terraform/cloud-docs/workspaces/health
 }
 
 ## 생성된 워크스페이스 데이터 소스 조회
-data "tfe_workspace" "tfc_poc_workspace" {
-  for_each = toset(var.workspace_names)
+# data "tfe_workspace" "tfc_poc_workspace" {
+#   for_each = toset(var.workspace_names)
 
-  name         = each.value
-  organization = var.TFC_ORGANIZATION_NAME
-  depends_on   = [tfe_workspace.tfc_poc_workspace]
-}
+#   name         = each.value
+#   organization = var.TFC_ORGANIZATION_NAME
+#   depends_on   = [tfe_workspace.tfc_poc_workspace]
+# }
 
 ## 모든 워크스페이스 remote 실행 모드 설정
 resource "tfe_workspace_settings" "tfc_poc_workspace_settings" {
   for_each = toset(var.workspace_names)
 
-  workspace_id   = data.tfe_workspace.tfc_poc_workspace[each.key].id
+  workspace_id   = tfe_workspace.tfc_poc_workspace[each.key].id
   execution_mode = "remote"
 }
