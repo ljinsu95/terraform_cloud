bcf561a52b9572432eaf59c5538ddada2097cc5b
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-10-02 12:42:37 +0900

    사실 vpc 세부 조회는 필요없었습니다.

--- import_test/output.tf
+++ import_test/output.tf
@@ -3,28 +3,28 @@ output "check_vpcs" {
   value = data.aws_vpcs.existing
 }
 
-# VPC 확인
-output "check_vpc" {
-  value = data.aws_vpc.existing
-}
-
-# 모든 VPC에 대한 Route Table 리스트 조회
-output "check_route_tables" {
-  value = data.aws_route_tables.existing
-}
-
-# VPC 별 모든 Route Table 조회
-output "check_route_table" {
-  value = data.aws_route_table.existing
-}
-# output "Nameasd" {
-#   value = tolist(data.aws_route_table.existing[*].tags)[0].Name
+# # VPC 확인
+# output "check_vpc" {
+#   value = data.aws_vpc.existing
 # }
-# output "Nameasd" {
-#   value = local.route
+
+# # 모든 VPC에 대한 Route Table 리스트 조회
+# output "check_route_tables" {
+#   value = data.aws_route_tables.existing
 # }
-output "check_route_id" {
-  value = local.route_map
-}
+
+# # VPC 별 모든 Route Table 조회
+# output "check_route_table" {
+#   value = data.aws_route_table.existing
+# }
+# # output "Nameasd" {
+# #   value = tolist(data.aws_route_table.existing[*].tags)[0].Name
+# # }
+# # output "Nameasd" {
+# #   value = local.route
+# # }
+# output "check_route_id" {
+#   value = local.route_map
+# }
 
 
