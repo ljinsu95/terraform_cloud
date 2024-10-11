95188c6e71f7f4e295c8f8ad76c717317c3eb312
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-10-11 15:11:28 +0900

    vpc endpoint 생성

---
+++ import_test/tgwatc.tf
@@ -0,0 +1,5 @@
+resource "aws_vpc_endpoint" "vpcend-s3" {
+  service_name    = "com.amazonaws.ca-central-1.s3"
+  vpc_id          = "vpc-0f8af692fead0eaea"
+  route_table_ids = ["rtb-05623991dbb664c96"]
+}
