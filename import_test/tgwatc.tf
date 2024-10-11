f59da122ae8028c960dec23d9e2f78c0bf58b097
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-10-11 15:22:23 +0900

    KB캐피탈 초기 세팅

--- import_test/tgwatc.tf
+++ import_test/tgwatc.tf
@@ -1,7 +1,8 @@
 resource "aws_vpc_endpoint" "vpcend-s3" {
   service_name    = "com.amazonaws.ca-central-1.s3"
   vpc_id          = "vpc-0f8af692fead0eaea"
-  route_table_ids = ["rtb-05623991dbb664c96"]
+  route_table_ids = [aws_route_table.rt-tf-import.id]
+  # route_table_ids = ["rtb-05623991dbb664c96"]
 
   tags = {
     Name = "vpcend-s3-tf-import"
