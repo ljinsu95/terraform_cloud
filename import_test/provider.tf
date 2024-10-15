01ab51c91d67abd2b68920f980fb9e5b1f8ce296
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-10-15 13:53:10 +0900

    provider 수정

--- import_test/provider.tf
+++ import_test/provider.tf
@@ -1,20 +1,28 @@
-### Provider
+# Provider
 provider "aws" {
-  ## AWS Region
+  ## AWS Provider 설정
   region     = var.aws_region
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-# remote 실행모드 설정
-# terraform login 필요
 terraform {
+  # remote 실행모드 설정 (로컬에서 실행 시 terraform login 필요)
   cloud {
-    hostname = "jinsu.terraform.insideinfo-cloud.com"
-    organization = "insideinfo"
+    # hostname     = "jinsu.terraform.insideinfo-cloud.com"
+    # organization = "insideinfo"
+    hostname     = "app.terraform.io"
+    organization = "insideinfo_jinsu"
 
     workspaces {
       name = "import_test"
+    }
+  }
+
+  required_providers {
+    # AWS Provider Version 설정
+    aws = {
+      version = ">=5.70.0"
     }
   }
 }
