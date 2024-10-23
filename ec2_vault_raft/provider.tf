807c448136561224ee63b2f4f512d0a0cc380aef
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-10-23 15:45:41 +0900

    remote 모드 설정

--- ec2_vault_raft/provider.tf
+++ ec2_vault_raft/provider.tf
@@ -9,15 +9,23 @@ provider "aws" {
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-## remote 실행모드 설정
-## terraform login 필요
-# terraform {
-#   cloud {
-#     organization = "insideinfo_jinsu"
+terraform {
+  # remote 실행모드 설정 (로컬에서 실행 시 terraform login 필요)
+  cloud {
+    # hostname     = "jinsu.terraform.insideinfo-cloud.com"
+    # organization = "insideinfo"
+    hostname     = "app.terraform.io"
+    organization = "insideinfo_jinsu"
 
-#     workspaces {
-#       # name = path.cwd
-#       name = "ec2_vault_raft"
-#     }
-#   }
-# }
+    workspaces {
+      name = "ec2_vault_raft"
+    }
+  }
+
+  required_providers {
+    # AWS Provider Version 설정
+    aws = {
+      version = ">=5.70.0"
+    }
+  }
+}
